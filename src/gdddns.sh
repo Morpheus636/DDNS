49a38e95464729e462d33ea02a82a924320ccb90
Josh Levin <morpheus636@morpheus636.com>
2022-06-27 15:18:11 -0400

    fixed typo

--- src/gdddns.sh
+++ src/gdddns.sh
@@ -7,7 +7,7 @@ GD_AUTH_STR="Authorization: sso-key $GD_API_KEY"
 
 ip_addr=$(curl -s "https://api.ipify.org")
 dns_data=$(curl -s -X GET -H "$GD_AUTH_STR" "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$HOSTNAME")
-gd_ip_addr=$(echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2)
+gd_ip_addr=$(echo $dns_data | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2)
 echo "Current external IP is $ip_addr, GoDaddy DNS IP is $gd_ip_addr"
 
 if [ "$gd_ip_addr" != "$ip_addr" -a $ip_addr != "" ]; then
